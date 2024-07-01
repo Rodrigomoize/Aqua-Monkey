53abd6b2e105fc7a07e18cbb0b7d1e3d3e10f9f8
Dídac Pérez Márquez <125640145+didacpema@users.noreply.github.com>
2024-07-01 17:41:06 +0200

    bubbles & underwater effects

--- SummerHorrorJam/Assets/UnderwaterEffects.cs
+++ SummerHorrorJam/Assets/UnderwaterEffects.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class UnderwaterEffects : MonoBehaviour
 {
     //[SerializeField] GameObject waterFx;
+    public AudioSource audioSource;
+    public AudioClip breathingUnderwater;
     public OxygenSystem oxigen;
+    public GameObject bubbles;
     public GameObject visor;
+
+    public bool underwater;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other){
         //waterFx.gameObject.SetActive(true);
+        underwater=true;
         RenderSettings.fog=true;
         oxigen.isConsumingOxigen=true;
         visor.gameObject.SetActive(true);
+        bubbles.gameObject.SetActive(true);
+        audioSource.PlayOneShot(breathingUnderwater);
+        audioSource.mute=false;
+
+
     }
 
     private void OnTriggerExit(Collider other){
         //waterFx.gameObject.SetActive(false);
+        underwater=false;
         RenderSettings.fog=false;
         oxigen.isConsumingOxigen=false;
         visor.gameObject.SetActive(false);
+        bubbles.gameObject.SetActive(false);
+        audioSource.mute=true;
     }
 }
